77348f941fd39cfc37f3d1067e6e8d550c66a6bf
ram276 <43284759+ram276@users.noreply.github.com>
2019-07-23 23:04:18 +0530

    Add files via upload

---
+++ db/pen.js
@@ -0,0 +1,23 @@
+let pen = [{
+        id: 1,
+        name: 'pen',
+        company: 'FEKO',
+        price: 10
+    },
+    {
+        id: 2,
+        name: 'pen',
+        company: 'KODE',
+        price: 100
+    },
+    {
+        id: 3,
+        name: 'pen',
+        company: 'EEE',
+        price: 105
+    }
+
+
+
+]
+module.exports.pen = pen;

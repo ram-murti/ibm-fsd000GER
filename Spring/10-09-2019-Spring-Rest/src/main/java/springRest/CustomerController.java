d83f962f54cb5417bdf4fe2bda28d9ce7289dfe1
b4ibmjava07@iiht.tech <b4ibmjava07@iiht.tech>
2019-09-10 13:23:58 +0530

    New Commit

--- Spring/10-09-2019-Spring-Rest/src/main/java/springRest/CustomerController.java
+++ Spring/10-09-2019-Spring-Rest/src/main/java/springRest/CustomerController.java
@@ -7,8 +7,12 @@ import org.jboss.logging.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,23 +30,28 @@ public class CustomerController {
 	{
 		return service.viewAllCustomer();
 	}
-	@GetMapping(value="/customer/{id}")
-	public Customer getCustomerById(int id) {
-		
+	@GetMapping(value="/customers/{id}")
+	public Customer getCustomerById(@PathVariable("id") String param) {
+		int id=Integer.parseInt(param);
 		return service.getCustomer(id);
 	}
 	
-	@PostMapping
-	public void addCustomer(Customer customer) {
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
 		
 	service.createCustomer(customer);
+	return customer;
 	}
-	@DeleteMapping(value="/?id")
-	public void deleteCustomer(int id) {
+	@DeleteMapping(value="/customers/{id}")
+	public void deleteCustomer(@PathVariable("id") String param) {
+		int id=Integer.parseInt(param);
+
 		service.deleteCustomer(id);
 	}
-	
-	public void updateCustomer() {
-		//service.updateEmploy(id, firstName, lastName, email);
+	@PutMapping("/customers/{id}")
+	public void updateCustomer(@PathVariable("id") String param,@RequestBody Customer customer) {
+		int id=Integer.parseInt(param);
+          System.out.println(customer.getEmail());
+		service.updateEmploy(id, customer.getFirstName(), customer.getLastName(), customer.getEmail());
 	}
 }

8081985ec8becc50c7e533a0d768e5ab9909ace6
b4ibmjava07@iiht.tech <b4ibmjava07@iiht.tech>
2019-09-16 17:12:25 +0530

    new changes

--- Spring/16-09-2019-microservices-client/src/main/java/com/example/demo/MyController.java
+++ Spring/16-09-2019-microservices-client/src/main/java/com/example/demo/MyController.java
@@ -1,9 +1,27 @@
 package com.example.demo;
 
-public class MyController {
 
-	public MyController() {
-		// TODO Auto-generated constructor stub
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+	@Autowired
+	private Environment env;
+	@Value("${ lucky-word}")
+	private String luckyWord;
+	private static final Logger logger=LoggerFactory.getLogger(MyController.class);
+	@RequestMapping("/lucky-word")
+	public String becomeLucky()
+	{
+		logger.info("config read from server.. running clitn at port no: "+env.getProperty("local.server.port"));
+		return "your lucky word is: "+luckyWord;
 	}
 
 }
